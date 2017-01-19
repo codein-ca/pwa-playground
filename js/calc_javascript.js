48b2b9690a4f7c94938b0f11eba21ca8bde2db15
Daniel Santos <daniel@otmo7.com>
2017-01-19 03:13:04 -0500

    No Bootstrap at all. Still to include some Jquery (next release).

--- js/calc_javascript.js
+++ js/calc_javascript.js
@@ -1,3 +1,12 @@
-/**
- * Created by DANIEL on 2016-12-29.
- */
+$("article>div:nth-child(1)>h3").click(function () {
+    $("article>div:nth-child(1)>p").slideToggle();
+});
+$("article>div:nth-child(2)>h3").click(function () {
+    $("article>div:nth-child(2)>p").slideToggle();
+});
+$("article>div:nth-child(3)>h3").click(function () {
+    $("article>div:nth-child(3)>p").slideToggle();
+});
+$("article>div:nth-child(4)>h3").click(function () {
+    $("article>div:nth-child(4)>p").slideToggle();
+});
