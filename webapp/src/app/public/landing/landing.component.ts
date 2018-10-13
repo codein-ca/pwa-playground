e5db22b678c0d6e8ac39e2e66bc80b84364450bf
Daniel Santos <daniel@brasileiro.ca>
2018-10-13 06:25:39 -0400

    Playing with images - Issue #19

--- webapp/src/app/public/landing/landing.component.ts
+++ webapp/src/app/public/landing/landing.component.ts
@@ -2,15 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'calc-landing',
-  template: `
-    <section class="calc-landing">
-      <h1 tabindex="-1">
-        Welcome. I am the only landing page!
-      </h1>
-    </section>
-
-  `,
-  styles: ['/*.calc-landing {min-height: 200vh}*/']
+  templateUrl: 'landing.component.html',
+  styleUrls: ['landing.component.scss']
 })
 export class LandingComponent implements OnInit {
 
